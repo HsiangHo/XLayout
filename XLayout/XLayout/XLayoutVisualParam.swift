c2996a45b4fa4e6a31b3649d274cc73b13d7cf96
HsiangHo <ho_cheung@qq.com>
2019-07-11 00:05:00 +0800

    implemented XLayoutVisualParam

--- XLayout/XLayout/XLayoutVisualParam.swift
+++ XLayout/XLayout/XLayoutVisualParam.swift
@@ -13,6 +13,67 @@ enum XLayoutVisualElement {
     case padding
 }
 
+public enum XLayoutDirection {
+    case vertical
+    case horizontal
+}
+
 public class XLayoutVisualParam {
     public var elementOrder: [Any] = []
+    public func constraintParamForView(firstItem: XLayoutView,
+                                       direction: XLayoutDirection = .horizontal) -> [XLayoutConstraintParam] {
+        var params: [XLayoutConstraintParam] = []
+        let firstItemIndex: Int? = elementOrder.firstIndex {
+            if let view = $0 as? XLayoutView {
+                return view === firstItem
+            }
+            return false
+        }
+
+        if let index = firstItemIndex {
+            if index >= 2 {
+                let paddingL = elementOrder[index - 1]
+                let secondItemL = elementOrder[index - 2]
+                if let param: XLayoutConstraintParam = handleParam(padding: paddingL, secondItem: secondItemL) {
+                    param.attribute = (direction == .horizontal ? .trailing : .bottom)
+                    params.append(param)
+                }
+
+            }
+
+            let paddingR = elementOrder[index + 1]
+            let secondItemR = elementOrder[index + 2]
+            if let param: XLayoutConstraintParam = handleParam(padding: paddingR, secondItem: secondItemR) {
+                param.attribute = (direction == .horizontal ? .leading : .top)
+                params.append(param)
+            }
+        }
+        return params
+    }
+
+    public func handleParam(padding: Any, secondItem: Any) -> XLayoutConstraintParam? {
+        var param: XLayoutConstraintParam?
+        if let value = padding as? XLayoutConstraintParam {
+            param = value.copy()
+        } else if let value = padding as? XLayoutVisualElement, value == .padding {
+            param = XLayoutConstraintParam.init()
+            param!.constant = 8
+        } else if let value = padding as? CGFloat {
+            param = XLayoutConstraintParam.init()
+            param!.constant = value
+        }
+
+        guard let constraintParam = param else {
+            return param
+        }
+
+        if let item = secondItem as? XLayoutVisualElement, item == .superView {
+            constraintParam.isSmart = true
+        } else if let item = secondItem as? XLayoutView {
+            constraintParam.secondItem = item
+        } else {
+            param = nil
+        }
+        return param
+    }
 }
